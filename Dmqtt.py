844e05add7c7de096a27a1c88374b585a061085a
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2019-01-09 03:43:03 +0000

    iServStor project - upload file

--- Dmqtt.py
+++ Dmqtt.py
@@ -1,12 +1,28 @@
+import ipfsapi
+import json
+import os
 import paho.mqtt.client as mqtt
+### Configuration
+Cpath = os.path.dirname(os.path.realpath(__file__))
+with open(Cpath+'/config.json') as f:
+    Jconfig = json.load(f)
+IPFS_IP = Jconfig['IPFS_IP']
+IPFS_PORT = Jconfig['IPFS_PORT']
+api = ipfsapi.connect(IPFS_IP,IPFS_PORT)
+
+TEST = "test"
+PIN_ADD = "pin_add"
 
 def on_connect(client, userdata,flags_dict, rc):
-    client.subscribe("test")
+    client.subscribe(TEST)
+    client.subscribe(PIN_ADD)
 
 def on_message(client, userdata, msg):
     msg.payload = msg.payload.decode("utf-8")
-    if msg.topic=='test':
+    if msg.topic==TEST:
         print(str(msg.payload))
+    if msg.topic==PIN_ADD:
+        api.pin_add(str(msg.payload))
 
 client = mqtt.Client()
 client.on_connect = on_connect
--- iservstor.py
+++ iservstor.py
@@ -12,16 +12,22 @@ class iServStor:
     ### Initialization
     def __init__(self):
         self.api = ipfsapi.connect(IPFS_IP,IPFS_PORT)
+        os.system("mkdir -p "+Cpath+"/"+Jconfig['DBpath'])
+        os.system("mkdir -p "+Cpath+"/"+Jconfig['FilePath'])
     def GetGoodPeers(self):
         Pdict = dict()
+        speers = self.GetSwarmPeers()
         conn = sqlite3.connect(Cpath+'/'+Jconfig['DBpath']+'/Iportal.db')
         c = conn.cursor()
         c.execute("SELECT * FROM Peers WHERE status = 0;") # IP, peerID, status, speed, nextTry
         for x in c:
+            if x[1] not in speers:
+                continue
             Pdict[x[1]] = dict()
             Pdict[x[1]]['IP'] = x[0]
             Pdict[x[1]]['status'] = x[2]
             Pdict[x[1]]['speed'] = x[3]
+            Pdict[x[1]]['nextTry'] = x[4]
         return Pdict
     def GetSwarmPeers(self):
         Pset = set()
@@ -29,3 +35,14 @@ class iServStor:
         for x in swarm:
             Pset.add(x['Peer'])
         return Pset
+    def FileUpload(self,Fname):
+        result = self.api.add(Cpath+'/'+Jconfig['FilePath']+'/'+Fname)
+        Ftype = "dictionary"
+        if 'Name' in result:
+            result = [result]
+            Ftype = "file"
+        for x in result:
+            if x['Name']==Fname:
+                x['type'] = Ftype
+                return x
+        return {"status": "Failed"}
